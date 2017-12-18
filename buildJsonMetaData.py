674e13059ffe2dd4b83ee3932711f294c165bf8f
Aaron Rosado <arosad2@protonmail.ch>
2017-12-18 14:58:49 -0500

    Altered formating.

--- buildJsonMetaData.py
+++ buildJsonMetaData.py
@@ -3,8 +3,6 @@ import os
 import sys
 import cv2
 
-
-
 imgSubjMetaData = {}
 
 subjRootDir = "rawImageData/"
@@ -14,16 +12,17 @@ for dirName, subdirList, fileList in os.walk(subjRootDir):
     imgSubjMetaData[dirName] = []
 
     for fname in fileList:
-    	## Get the image dimensions as well
-		imagePath = dirName + "/" +fname
-		try:
-			# image = cv2.imread(imagePath)
-			# (h, w) = image.shape[:2]
-			(h ,w ) = (256,256)
-			fileMetaData = { "label": dirName.replace(subjRootDir,""), 'filename': fname, 'fileWpath': imagePath, 'imgDimensions': (h,w) }
-			imgSubjMetaData[dirName].append(fileMetaData)
-		except:
-			print "Having some issue with",imagePath
+        ## Get the image dimensions as well
+        imagePath = dirName + "/" + fname
+        try:
+            # image = cv2.imread(imagePath)
+            # (h, w) = image.shape[:2]
+            (h, w) = (256, 256)
+            fileMetaData = {"label": dirName.replace(subjRootDir, ""), 'filename': fname, 'fileWpath': imagePath,
+                            'imgDimensions': (h, w)}
+            imgSubjMetaData[dirName].append(fileMetaData)
+        except:
+            print "Having some issue with", imagePath
 
 print imgSubjMetaData
 ## This is currently a dictionary, need it to be flattened into a list of dictionaries so it's
@@ -32,11 +31,11 @@ print imgSubjMetaData
 
 simpsonsImgMetaData = []
 
-for k,v in imgSubjMetaData.iteritems():
-	## Going to generate a "row" for each Class
-	if len(v) > 0:
-		classData = { "label": k.replace(subjRootDir,""), "imgsInClass": len(v), "imgSet": v, "sampleImg": v[0]}
-		simpsonsImgMetaData.append(classData)
+for k, v in imgSubjMetaData.iteritems():
+    ## Going to generate a "row" for each Class
+    if len(v) > 0:
+        classData = {"label": k.replace(subjRootDir, ""), "imgsInClass": len(v), "imgSet": v, "sampleImg": v[0]}
+        simpsonsImgMetaData.append(classData)
 
-with open("web/simpsonsImgMetaData.json","w") as fp:
-	json.dump(simpsonsImgMetaData,fp)
+with open("web/simpsonsImgMetaData.json", "w") as fp:
+    json.dump(simpsonsImgMetaData, fp)
